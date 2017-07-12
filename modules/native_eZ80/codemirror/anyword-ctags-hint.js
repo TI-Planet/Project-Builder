24d063a5271045f2f85329ee42dabdd13230d9b6
Adrien Bertrand <bertrand.adrien@gmail.com>
2017-07-13 01:49:25 +0200

    [CE] Rework order of search for autocompletions + sorting
    Thanks to @Runer112 for the sorting logic

--- modules/native_eZ80/codemirror/anyword-ctags-hint.js
+++ modules/native_eZ80/codemirror/anyword-ctags-hint.js
@@ -28,37 +28,63 @@
 
       if (curWord)
       {
-          // from anywhere, non fuzzy
-          var re = new RegExp(word.source, "gi");
-          for (var dir = -1; dir <= 1; dir += 2) {
-              var line = cur.line, endLine = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
-              for (; line != endLine; line += dir) {
-                  var text = editor.getLine(line), m;
-                  while (m = re.exec(text)) {
-                      if (line == cur.line && m[0] === curWord) continue;
-                      if ((!curWord || m[0].lastIndexOf(curWord, 0) == 0) && !Object.prototype.hasOwnProperty.call(seen, m[0])) {
-                          seen[m[0]] = true;
-                          list.push(m[0]);
+          const upperCaseCurWord = curWord.toUpperCase();
+
+          const isEditorASM = editor.getMode().name === 'z80';
+
+          if (!isEditorASM)
+          {
+              // from anywhere, non fuzzy
+              var re = new RegExp(word.source, "gi");
+              for (var dir = -1; dir <= 1; dir += 2) {
+                  var line = cur.line, endLine = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
+                  for (; line != endLine; line += dir) {
+                      var text = editor.getLine(line), m;
+                      while (m = re.exec(text)) {
+                          if (line == cur.line && m[0] === curWord) continue;
+                          if ((!curWord || m[0].lastIndexOf(curWord, 0) == 0) && !Object.prototype.hasOwnProperty.call(seen, m[0])) {
+                              seen[m[0]] = true;
+                              list.push(m[0]);
+                          }
                       }
                   }
               }
           }
-          list = list.sort();
-
 
           const fuzzyRegex = new RegExp(curWord.split("").reduce((a, b) => a + '[^' + b + ']*' + b), 'gi');
+
           // from ctags, fuzzy
           if (typeof(window.ctags) === 'object') {
               Array.prototype.push.apply(list, window.ctags.filter((val) => val.n.match(fuzzyRegex)).map((val) => val.n).sort());
           }
-          // from sdk_ctags, fuzzy
-          if (typeof(window.sdk_ctags) === 'object' && window.enable_sdk_ctags) {
-              Array.prototype.push.apply(list, window.sdk_ctags.filter((val) => val.n.match(fuzzyRegex)).map((val) => val.n).sort());
+          if (!isEditorASM)
+          {
+              // from sdk_ctags, fuzzy
+              if (typeof(window.sdk_ctags) === 'object' && window.enable_sdk_ctags) {
+                  Array.prototype.push.apply(list, window.sdk_ctags.filter((val) => val.n.match(fuzzyRegex)).map((val) => val.n).sort());
+              }
           }
-          // from ti84pceInc_ctags, fuzzy
-          if (typeof(window.ti84pceInc_ctags) === 'object' && !window.enable_ti84pceInc_ctags) {
-              Array.prototype.push.apply(list, window.ti84pceInc_ctags.filter((val) => val.n.match(fuzzyRegex)).map((val) => val.n).sort());
+          if (isEditorASM)
+          {
+              // from ti84pceInc_ctags, fuzzy
+              if (typeof(window.ti84pceInc_ctags) === 'object' && !window.enable_ti84pceInc_ctags) {
+                  Array.prototype.push.apply(list, window.ti84pceInc_ctags.filter((val) => val.n.match(fuzzyRegex)).map((val) => val.n).sort());
+              }
           }
+
+          list = list.sort( (str1, str2) => {
+              str1 = str1.toUpperCase();
+              str2 = str2.toUpperCase();
+              const isPrefix1 = str1.startsWith(upperCaseCurWord);
+              const isPrefix2 = str2.startsWith(upperCaseCurWord);
+              if (isPrefix1 && !isPrefix2) {
+                  return -1;
+              }
+              if (isPrefix2 && !isPrefix1) {
+                  return 1;
+              }
+              return str1 < str2 ? -1 : str1 > str2 ? 1 : 0;
+          });
       }
 
       // remove duplicates
