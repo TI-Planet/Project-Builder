7c3daa3fe9fa4f0eeebfd5e4c2d67aef53220079
Lionel Debroux <lionel_debroux@yahoo.fr>
2017-11-15 15:14:01 +0100

    Add a first version of a Vagrant-based test setup, plus several stubs for SQLite.

---
+++ SQLite_UserInfoProvider.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Part of TI-Planet's Project Builder
+ * (C) Adrien "Adriweb" Bertrand
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+namespace ProjectBuilder;
+
+require_once 'IUserInfoProvider.php';
+
+final class SQLite_UserInfoProvider implements IUserInfoProvider
+{
+
+    public static function getConnectedUserInfo()
+    {
+    }
+
+    /**
+     * @param int $userID
+     * @return UserInfo
+     */
+    public static function getUserInfoFromID($userID = 1)
+    {
+    }
+}
---
+++ dbconfig_SQLite.php
@@ -0,0 +1,8 @@
+<?php
+$dbms = '';
+$dbhost = '127.0.0.1';
+$dbport = '';
+$dbname = '';
+$dbuser = '';
+$dbpasswd = '';
+?>
