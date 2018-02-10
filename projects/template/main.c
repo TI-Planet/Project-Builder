1d2b9bdcc6932d6033c5166a8ab7738ab6fb9c9c
Adrien Bertrand <bertrand.adrien@gmail.com>
2018-02-11 04:47:57 +0800

    [CE C] Fix printTextSmall in template
    Needs os_FontSelect(0); first to be sure that the small font will be
    used.

--- projects/template/main.c
+++ projects/template/main.c
@@ -55,6 +55,7 @@ void printText(const char *text, uint8_t xpos, uint8_t ypos)
 /* Draw small text at the given X/Y location */
 void printTextSmall(const char *text, uint8_t xpos, uint8_t ypos)
 {
+    os_FontSelect(0); // sets small font (1 == big, see docs)
     os_FontDrawText(text, xpos, ypos);
 }
 
