7005ba067dc917be412182c2c094382dbd623194
Adrien Bertrand <bertrand.adrien@gmail.com>
2019-05-25 17:43:02 +0200

    Update CE C code template

--- projects/template/main.c
+++ projects/template/main.c
@@ -19,17 +19,15 @@
 
 /* Other available headers: stdarg.h, setjmp.h, assert.h, ctype.h, float.h, iso646.h, limits.h, errno.h */
 
+/* uint8_t is an unsigned integer that can range from 0-255. */
+/* It performs faster than just an int, so try to use it (or int8_t) when possible */
+
 /* Put function prototypes here */
-void printText(const char *text, uint8_t xpos, uint8_t ypos);
-void printTextSmall(const char *text, uint8_t xpos, uint8_t ypos);
+void printText(const char *text, uint8_t x, uint8_t y);
 
 /* Put all your code here */
 void main(void)
 {
-    /* uint8_t is an unsigned integer that can range from 0-255. */
-    /* It performs faster than just an int, so try to use it (or int8_t) when possible */
-    uint8_t count;
-
     /* Initialize some strings */
     const char* HelloWorld = "Hello World!";
     const char* Welcome    = "Welcome to C!";
@@ -37,7 +35,7 @@ void main(void)
     /* Clear the homescreen */
     os_ClrHome();
 
-    /* Display a few strings */
+    /* Print a few strings */
     printText(HelloWorld, 0, 0);
     printText(Welcome, 0, 1);
 
@@ -45,17 +43,11 @@ void main(void)
     while (!os_GetCSC());
 }
 
-/* Draw text on the homescreen at the given X/Y location */
+/* Draw text on the homescreen at the given X/Y cursor location */
 void printText(const char *text, uint8_t xpos, uint8_t ypos)
 {
     os_SetCursorPos(ypos, xpos);
     os_PutStrFull(text);
 }
 
-/* Draw small text at the given X/Y location */
-void printTextSmall(const char *text, uint8_t xpos, uint8_t ypos)
-{
-    os_FontSelect(0); // sets small font (1 == big, see docs)
-    os_FontDrawText(text, xpos, ypos);
-}
 
