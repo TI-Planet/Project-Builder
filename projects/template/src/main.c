8ae26698c06912381f1827504cb3b7a9d931b254
Adrien Bertrand <bertrand.adrien@gmail.com>
2021-08-21 17:43:11 -0400

    [CE] Improve default C project template.

--- projects/template/src/main.c
+++ projects/template/src/main.c
@@ -5,28 +5,35 @@
 // Description:
 ////////////////////////////////////////
 
-/* Keep these headers */
+/*
+* The comments in this file are here to guide you initially. Note that you shouldn't actually
+* write comments that are pointless or obvious in your own code, write useful ones instead!
+* See this for more details: https://ce-programming.github.io/toolchain/static/coding-guidelines.html
+*
+* Have fun!
+*/
+
+/* You probably want to keep these headers */
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <tice.h>
 
-/* Standard headers */
+/* Here are some standard headers. Take a look at the toolchain for more. */
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/* Other available headers: stdarg.h, setjmp.h, assert.h, ctype.h, float.h, iso646.h, limits.h, errno.h */
+/* Put function prototypes here or in a header (.h) file */
 
-/* uint8_t is an unsigned integer that can range from 0-255. */
+/* Note: uint8_t is an unsigned integer that can range from 0-255. */
 /* It performs faster than just an int, so try to use it (or int8_t) when possible */
-
-/* Put function prototypes here */
 void printText(const char *text, uint8_t x, uint8_t y);
 
-/* Put all your code here */
-void main(void)
+/* This is the entry point of your program. */
+/* argc and argv can be there if you need to use arguments, see the toolchain example. */
+int main(void)
 {
     /* Initialize some strings */
     const char* HelloWorld = "Hello World!";
@@ -41,6 +48,8 @@ void main(void)
 
     /* Wait for a key press */
     while (!os_GetCSC());
+
+    return 0;
 }
 
 /* Draw text on the homescreen at the given X/Y cursor location */
@@ -49,5 +58,4 @@ void printText(const char *text, uint8_t xpos, uint8_t ypos)
     os_SetCursorPos(ypos, xpos);
     os_PutStrFull(text);
 }
-
 
